f0e48dbf57c36b2a70ad4c1a93a2916b1b399576
Lourdes Koval <lourdeskoval@MacBook-Pro-de-Lourdes.local>
2024-11-14 13:50:12 -0300

    ejercicio3.js

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,3 +1,13 @@
+/*Enunciado:
+Este ejercicio te ayudará a practicar la entrada de datos y el uso de operadores
+aritméticos. Escribe un programa que:
+1. Pida al usuario que ingrese dos números.
+2. Sume ambos números.
+3. Muestre el resultado de la suma.
+Requisitos:
+• Utiliza prompt() para recibir los números.
+• Utiliza parseFloat() para convertir las entradas a números.
+• Almacena los resultados en variables adecuadas y muestra el resultado.*/
 const prompt = require("prompt-sync")({ sigint: true });
 let numero = parseFloat(prompt("escribe un numero"));
 let numero2 = parseFloat(prompt("escribe otro numero"));
